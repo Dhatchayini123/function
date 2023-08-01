eccbd411aed944c8b060ba91c130c3ba2ad4b44f
Dhatchayini123 <138356513+Dhatchayini123@users.noreply.github.com>
2023-08-01 22:44:00 +0530

    Add files via upload

---
+++ args pass.py
@@ -0,0 +1,6 @@
+def my_fun(fname):
+    print(fname +" perfect")
+#arguments will pass(practice makes perfect,today is perfect,success is perfect)
+my_fun("practice makes")
+my_fun("today is")
+my_fun("success is")
